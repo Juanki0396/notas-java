ad5a6ccad0a61f01109e39498759045da269f906
Juan Carlos Aranda Huecas <juanki0396@gmail.com>
2026-02-03 08:57:41 +0100

    Clase de estadistica hecha y testeada. Prototipo de app creado

--- src/main/java/pio/daw/stats/Estadistica.java
+++ src/main/java/pio/daw/stats/Estadistica.java
@@ -4,24 +4,66 @@ import java.util.List;
 
 public class Estadistica {
 
-    //TODO
+    /**
+     * Calcula la media de una muestra
+     * @param nums Muestra de numeros
+     * @return Media 
+     */
     public static Double media(List<Double> nums){
-        return 0.;
+        Double suma = 0.;
+        if(nums.size() == 0){
+            return suma;
+        }
+        for(Double n : nums){
+            suma += n;
+        }
+        return suma / nums.size();
     }
 
-    //TODO
+    
+    /**
+     * Calcula el máximo de una muestra
+     * @param nums Muestra de numeros
+     * @return Máximo 
+     */
      public static Double max(List<Double> nums){
-        return 0.;
+        Double maxNum = nums.get(0);
+        for(Double n: nums){
+            if (n > maxNum){
+                maxNum = n;
+            }
+        }
+        return maxNum;
     }
 
-    //TODO
+    
+    /**
+     * Calcula el mínimo de una muestra
+     * @param nums Muestra de numeros
+     * @return Mínimo 
+     */
      public static Double min(List<Double> nums){
-        return 0.;
+        Double minNum = nums.get(0);
+        for(Double n: nums){
+            if (n < minNum){
+                minNum = n;
+            }
+        }
+        return minNum;
     }
 
-    //TODO
+    /**
+     * Calcula la varianza de una muestra
+     * @param nums Muestra
+     * @return Varianza
+     */
      public static Double var(List<Double> nums){
-        return 0.;
+        Double suma = 0.;
+        Double media = media(nums);
+        for(Double n : nums){
+            suma += Math.pow(n - media, 2);
+        }
+        return suma / (nums.size());
     }
     
 }
